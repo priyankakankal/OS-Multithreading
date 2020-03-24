29413533ad87c57b88254965e048415584a66223
priyankakankal <priyankakankal17@google.com>
2020-03-24 22:42:23 +0530

    create using one-one model

--- thread.c
+++ thread.c
@@ -1,13 +1,79 @@
-#include <malloc.h>
+#define _GNU_SOURCE
+#include <sys/wait.h>
+#include <sys/utsname.h>
+#include <sched.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <signal.h>
 #include <errno.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include "thread.h"
 
 int thread_create(thread_t *t, const thread_attr_t * attr, void * (*start_function)(void *), void *arg) {
 	//one-one and many-one model
+
+	thread_t tid;
+	unsigned long stack_size;
+	char *tchild_stack; // Pointer to stack of child thread
+	char *stackTop;
+	thread_struct *child_thread;
+
+	if(!thread_l_head) {
+		/* first create call
+			Initialize linked list of thread control block
+		*/
+	}
+
+
+	/* Create stack for child thread
+	*/
+	if (attr == NULL)
+		stack_size = SIGSTKSZ;
+	else
+		stack_size = attr->stack_size;
+
+	tchild_stack = malloc(stack_size);
+	if ( tchild_stack == NULL ) {
+		printf("ERROR: Unable to allocate memory for stack.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	stackTop = tchild_stack + stack_size;
+
+	/*
+		Create thread_struct for child thread
+	*/
+	child_thread = (thread_struct *)malloc(sizeof(thread_struct));
+	if (child_thread == NULL ) {
+		perror("ERROR: Unable to allocate memory for thread_struct.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	child_thread->start_func = start_function;
+	child_thread->arg = arg;
+	child_thread->state = READY;
+	child_thread->returnValue = NULL;
+	child_thread->blockedForJoin = NULL;
+
+	/* Add created thread_struct to list of thread blocks
+	*/
+	addthread_l(child_thread);
+
+	tid = clone(fn, stackTop, SIGCHLD, NULL);
+
+	if ( tid < 0 ) {
+        printf("ERROR: Unable to create the child process.\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+	child_thread->tid = tid;
+	
 	return 0;
 }
 
@@ -16,6 +82,17 @@ int thread_join(thread_t thread, void **retval) {
 
 
 /*for testing only*/
+int fn() {
+   printf("\nINFO: This code is running under child process.\n");
+
+   return 0;
+}
+
 int main() {
+	printf("%p\n", thread_l_head);
+	thread_t tid;
+
+	thread_create(&tid, NULL, fn, NULL);
+	
 	return 0;
 }
--- thread.h
+++ thread.h
@@ -12,10 +12,10 @@
 #define BLOCKED 2 /* Waiting on Join */
 #define DEFUNCT 3 /* Dead */
 
-typedef unsigned long int thread_t;
+typedef pid_t thread_t;
 
 typedef struct thread_attr_t {
-	unsigned long stackSize;     /* Stack size to be used by this thread. Default is SIGSTKSZ */
+	unsigned long stack_size;     /* Stack size to be used by this thread. Default is SIGSTKSZ */
 } thread_attr_t;
 
 
@@ -24,20 +24,21 @@ typedef struct thread_struct {
 	thread_t tid; 			/* The thread-id of the thread */
 	int state; 			/* the state in which the corresponding thread will be. */
 	void * (*start_func) (void *); 	/* The func pointer to the thread function to be executed. */
-	void *args; 			/* The arguments to be passed to the thread function. */
+	void *arg; 			/* The arguments to be passed to the thread function. */
 	void *returnValue; 			/* The return value that thread returns. */
 	struct thread_struct *blockedForJoin; 	/* Thread blocking on this thread */
 	struct thread_struct *prev, *next; 
 } thread_struct;
 
+
+/*
+	Thread library interface for user
+*/
 int thread_create(thread_t *t, const thread_attr_t * attr, void * (*start_function)(void *), void *arg); // provide option to use a desired mapping.
 
 int thread_join(thread_t thread, void **retval);
 
 void thread_exit(void *retval);
-
-
-
 /*
 
 thread_lock(); // a spinlock
@@ -45,4 +46,29 @@ thread_unlock();  // spin-unlock
 thread_kill();
 
 */
+
+
+// Global pointer to the head node in the queue of Thread Structure
+thread_struct *thread_l_head;
+
+/*
+	Internal thread function
+*/
+void addthread_l(thread_struct *node) {
+	thread_struct *tmp;
+
+	if(thread_l_head == NULL) {
+		thread_l_head = node;
+		node->next = node->prev = node;
+		return;
+	}
+
+
+	thread_l_head->prev->next = node;
+	node->prev = thread_l_head->prev;
+	node->next = thread_l_head;
+	thread_l_head->prev = node;
+
+	return;
+}
 
